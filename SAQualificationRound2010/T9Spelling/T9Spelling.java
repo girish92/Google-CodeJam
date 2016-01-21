099a58fb7c1af9692f730405ed9dba5f8ecbfce8
Girish <girish92_omsairam@yahoo.com.sg>
2016-01-22 01:29:34 +0800

    Added test data for T9Spelling program

--- SAQualificationRound2010/T9Spelling/T9Spelling.java
+++ SAQualificationRound2010/T9Spelling/T9Spelling.java
@@ -50,3 +50,67 @@ Case #3: 333666 6660 022 2777
 Case #4: 4433555 555666096667775553
 
 */
+
+
+import java.util.*;
+
+class T9Spelling{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int tests = sc.nextInt();
+    sc.nextLine();
+
+    genKeyPresses(sc, tests);
+
+  }
+
+  public static void genKeyPresses(Scanner sc, int cases){
+
+    int count = 1;
+    HashMap<Character, String> map = new HashMap<Character, String>();
+    map.put(' ', "0");
+    map.put('a', "2");
+    map.put('b', "22");
+    map.put('c', "222");
+    map.put('d', "3");
+    map.put('e', "33");
+    map.put('f', "333");
+    map.put('g', "4");
+    map.put('h', "44");
+    map.put('i', "444");
+    map.put('j', "5");
+    map.put('k', "55");
+    map.put('l', "555");
+    map.put('m', "6");
+    map.put('n', "66");
+    map.put('o', "666");
+    map.put('p', "7");
+    map.put('q', "77");
+    map.put('r', "777");
+    map.put('s', "7777");
+    map.put('t', "8");
+    map.put('u', "88");
+    map.put('v', "888");
+    map.put('w', "9");
+    map.put('x', "99");
+    map.put('y', "999");
+    map.put('z', "9999");
+
+    while(cases > 0){
+      String str = sc.nextLine();
+      String res = new String();
+      for(int i = 0; i < str.length(); i++){
+        char c = str.charAt(i);
+        String val = map.get(c);
+        if(res.length() != 0 && res.charAt(res.length()-1) == val.charAt(0)){
+          res = res.concat(" ");
+        }
+        res = res.concat(val);
+      }
+      System.out.println("Case #" + count + ": " + res);
+      cases--;
+      count++;
+    }
+
+  }
+}
